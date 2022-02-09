318bf87d1b3aaf85c2e7a8b335d43a8c9aed9402
YoannaPo <yoannna.pol@gmail.com>
2022-02-09 12:47:40 +0000

    Add exercise 6

--- src/6_remove_double.js
+++ src/6_remove_double.js
@@ -1,5 +1,30 @@
-function removeDouble() {
-  // TODO Implement me.
+function removeDouble(myStuff) {
+
+    //const myStuff = (mixedStuff);
+  
+    console.log("myStuffArr tu leci: "+ myStuff);
+
+    for (let i = 0; i < myStuff.length; i++) {
+      console.log("Szukam i: " + i);
+      console.log("Szukam i: " + myStuff[i]);
+      for (let j = i+1; j < myStuff.length; j++) {
+        console.log('compare i to j:'+i+' '+j);
+        console.log('compare i to j:'+myStuff[i]+' '+myStuff[j]);
+        if(myStuff[i] === myStuff[j]) {
+          console.log("Pętla before: " + myStuff);
+          myStuff.splice(i, 1); // 2nd parameter means remove one item only
+          console.log("Pétla after: " + myStuff);
+          i--;
+          
+        }
+        
+      }
+    }
+   console.log('myStuff final: '+myStuff);
+
+   return(myStuff);
+ 
+  
 };
 
 
